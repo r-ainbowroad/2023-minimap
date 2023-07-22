ae79bb2c14606073a61bad0d7aed8c9fd24a579d
Fluttershy <fluttershy@pony.dev>
2023-07-21 22:48:01 -0700

    Add all book pages

---
+++ .build/template_assembler/gen_pages.py
@@ -0,0 +1,17 @@
+for i in range(27, 0, -1):
+    time = 1690002000 + 7400 * (i - 1)
+    page = i
+    print(f"""\
+    {{
+      "name": "book page {page}",
+      "enabled_utc": {time},
+      "images": [
+        "../mlp/source/book_frame{page:02d}.png"
+      ],
+      "x": 451,
+      "y": 712,
+      "priority": 8,
+      "autopick": true,
+      "export_group": "starter"
+    }},\
+    """)
