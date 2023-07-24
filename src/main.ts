c2f8433ef1d77364d8a353bdb496ef6716984faa
Dusk Banks <me@bb010g.com>
2023-07-24 16:30:15 -0700

    minimap: re-enable world

--- src/main.ts
+++ src/main.ts
@@ -25,6 +25,7 @@ const autoPickAfterPlaceTimeout = 3000;
 
   const blobServer = new BlobServer("https://ponyplace.z19.web.core.windows.net");
   minimap.templates.add("mlp_alliance", blobServer.getTemplate("mlp_alliance", {autoPick: true, mask: true}));
+  minimap.templates.add("mlp_world", blobServer.getTemplate("mlp_world", {autoPick: true, mask: true}));
   minimap.templates.add("mlp", blobServer.getTemplate("mlp", {autoPick: true, mask: true}));
 
   await minimap.initialize();
